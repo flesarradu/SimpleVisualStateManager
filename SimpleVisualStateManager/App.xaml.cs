b6b20e7aa3552cbcc31bc7c8a126ca7876ca1661
Flesar Radu <flesarradu@gmail.com>
2024-04-23 13:55:34 +0300

    Initial commit

---
+++ SimpleVisualStateManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleVisualStateManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
